3935b00be2955a6389766537bde7097ceba8928c
OliverioJunior <oliverio.junior2@gmail.com>
2025-09-20 16:17:32 -0300

    feat(auth): add login mutation with response type

--- packages/backend/src/typeDefs/mutation.ts
+++ packages/backend/src/typeDefs/mutation.ts
@@ -3,5 +3,13 @@ import gql from 'graphql-tag'
 export default gql`
   type Mutation {
     mutationTest(test: Boolean): Boolean
+    login(email: String!, password: String!): LoginResponse
+
+    type LoginResponse {
+    id: String
+    name: String
+    email: String
+    company: String
+    }
   }
 `
