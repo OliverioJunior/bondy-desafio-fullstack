74631e79c0a41c64b2cec03028a3b25b2dacea8b
OliverioJunior <oliverio.junior2@gmail.com>
2025-09-25 23:43:52 -0300

    refactor(typeDefs): restructure LoginResponse and User types

--- packages/backend/src/typeDefs/mutation.ts
+++ packages/backend/src/typeDefs/mutation.ts
@@ -4,12 +4,17 @@ export default gql`
   type Mutation {
     mutationTest(test: Boolean): Boolean
     login(email: String!, password: String!): LoginResponse
+  }
 
-    type LoginResponse {
-    id: String
-    name: String
-    email: String
+  type LoginResponse {
+    token: String!
+    user: User!
+  }
+
+  type User {
+    id: String!
+    name: String!
+    email: String!
     company: String
-    }
   }
 `
