ea750b31491ab318bdad120e2561cee7466ada9c
OliverioJunior <oliverio.junior2@gmail.com>
2025-09-25 23:44:15 -0300

    refactor(frontend): replace App component with router and providers
    Replace the basic App component with a new routing setup using AppRouter and wrap it with Apollo and Auth providers to enable client-side routing and state management

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ApolloProvider } from '@apollo/client/react'
+import { AuthProvider } from './contexts/useAuthContext'
+import { AppRouter } from './provider/route'
 import './styles/global.css'
-import App from './App'
+import client from './lib/apollo'
 
 // eslint-disable-next-line no-undef
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
+    </ApolloProvider>
   </React.StrictMode>
 )
